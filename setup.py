984a66cc73f4e68dc9aececbde65bca1a54f5b91
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-01-02 19:06:29 -0500

    add setup.py and update .gitignore

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bridges_uncc",
+    version="2.4.0",
+    author="BRIDGES",
+    author_email="bridges.uncc@gmail.com",
+    description="Bridging Real-world Infrastructure Designed to Goal-align, Engage, and Stimulate, an NFS TUES",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/krs-world/bridges-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
