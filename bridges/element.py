e0cb975417bde806be3e8ab7e5f38bc5b8a6759c
mmcquaig <mmcquaig@uncc.edu>
2019-01-03 19:43:25 -0500

    changed element class for kdtree

--- bridges/element.py
+++ bridges/element.py
@@ -2,6 +2,7 @@
 from bridges.element_visualizer import *
 from bridges.color import *
 from bridges.link_visualizer import *
+from bridges.kd_tree_element import *
 import re
 from decimal import Decimal
 ##
@@ -192,13 +193,17 @@ class Element(object):
         loc_flag = not ((self.visualizer.get_locationX() == Decimal('Infinity')) or (self.visualizer.get_locationY() == Decimal('Infinity')))
         if loc_flag:
              json_str += self.COMMA + self.QUOTE + "location" + self.QUOTE + self.COLON + self.OPEN_BOX + str(self.visualizer.get_locationX()) + self.COMMA + str(self.visualizer.get_locationY()) + self.CLOSE_BOX
-        # if self.get_data_struct_type() == "BinarySearchTree":
+        if self.get_data_struct_type() == "KdTree":
+            kdt = KDTreeElement(self)
+            json_str += self.COMMA + self.QUOTE + "key" + self.QUOTE + self.COLON + self.QUOTE + str(kdt.get_key()) + self.QUOTE + self.COMMA + self.QUOTE + "dimension" + self.QUOTE + self.COLON + self.QUOTE + kdt.get_dimension() + self.QUOTE + self.COMMA + self.QUOTE + "thickness" + self.QUOTE + self.COLON + self.QUOTE + kdt.get_thickness() + self.QUOTE + self.COMMA
+        #if self.get_data_struct_type() == "BinarySearchTree":
         #     bst = BSTElement(self)
         #     json_str += self.COMMA + self.QUOTE + "key" + self.QUOTE + self.COLON + self.QUOTE + bst.getKey().__str__() + self.QUOTE + self.COMMA
         # elif self.get_data_struct_type() == "AVLTree":
         #     json_str += self.COMMA + self.QUOTE + "key" + self.QUOTE + self.COLON + self.QUOTE + avl.getKey().__str__() + self.QUOTE + self.COMMA + self.QUOTE + "height" + self.QUOTE + self.COLON + Integer.toString(avl.getHeight()) + self.COMMA + self.QUOTE + "balance_factor" + QUOTE + COLON + Integer.toString(avl.getBalanceFactor()) + COMMA
         # else:
-        json_str += self.CLOSE_CURLY
+        else:
+            json_str += self.CLOSE_CURLY
         return json_str
 
     def get_link_representation(self, lv, src, dest):
