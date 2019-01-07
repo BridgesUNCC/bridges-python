0848aaa858fb015f0f8c50e0bf9d74d1b2e7f5e1
mmcquaig <mmcquaig@uncc.edu>
2019-01-07 12:07:19 -0500

    finished shape collection test

--- tests/shape_collection.py
+++ tests/shape_collection.py
@@ -2,17 +2,35 @@ from bridges.symbol_collection import *
 from bridges.bridges import *
 from bridges.circle import *
 from bridges.polygon import *
+from bridges.rectangle import *
 from bridges.label import *
 from bridges.color import *
 
 def main():
     bridges = Bridges(0, "test", "211416381091")
     bridges.connector.set_server("local")
+    bridges.set_title("Symbol Collection")
+    bridges.set_description("Some Shapes and Symbols")
     bridges.json_flag = True
+
     sc = SymbolCollection()
+    s1 = Rectangle(-25.0, 0.0, 50, 50)
+    s1.set_fill_color(Color("green"))
+    sc.add_symbol(s1)
+
     s2 = Circle(25.0, 0.0, 25)
     s2.set_fill_color(Color("green"))
     sc.add_symbol(s2)
+
+    s3 = Polygon()
+    s3.add_point(-100,0)
+    s3.add_point(100, 0)
+    s3.add_point(0, 0)
+    s3.add_point(0, -100)
+    s3.add_point(0, 100)
+    s3.add_point(0, 0)
+    s3.set_stroke_color(Color("magenta"))
+    sc.add_symbol(s3)
 
     l = Label()
     l.set_location(0, 25)
