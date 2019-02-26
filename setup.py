85194cee4475d83972758c14f05e9616bc6f06ca
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-02-26 14:08:16 -0500

    update setup.py for packaging

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bridges",
-    version="2.4.1",
+    version="2.4.1.3",
     author="BRIDGES",
     author_email="bridges.uncc@gmail.com",
     description="Bridging Real-world Infrastructure Designed to Goal-align, Engage, and Stimulate, an NFS TUES",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/krs-world/bridges-python",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests")),
     python_requires='>3.5.2',
     install_requires=[
-        'requests',
-        'webcolors',
+        'requests>=2.21.0',
+        'webcolors>=1.8.1',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
