d136cc117d64177ccb419af8a7d512c3ec0faa69
quaiquai <mmcquaig@uncc.edu>
2019-07-11 16:11:16 -0400

    added game_cell, keypress listener, and fixed socket connection

--- bridges/socket_connection.py
+++ bridges/socket_connection.py
@@ -1,22 +1,62 @@
 import socketio
 
+
 class SocketConnection:
     _sio = socketio.Client()
     _listeners = []
 
+    @_sio.event
     def add_listener(self, to_add):
         print("Subscribing to keypress events..")
         self._listeners.append(to_add)
 
+    @_sio.event
     def setup_connection(self, user, assignment):
         try:
             url = "https://bridges-games.herokuapp.com"
             self._sio.connect(url)
 
-            @self._sio.on('on')
-            def
+            student_cred = {
+                'user': user,
+                'assignment': assignment.split('.')[0]
+            }
+
+            self._sio.emit('credentials', student_cred)
+
         except ConnectionError as e:
             print(e)
+
+    @_sio.event
+    def connect(self):
+        print("Connected")
+
+    @_sio.on('keydown')
+    def keydown(self, args):
+        obj = args[0]
+        for i in range(len(self._listeners)):
+            self._listeners[i].keypress(obj)
+
+    @_sio.on('keyup')
+    def keyup(self, args):
+        obj = args[0]
+        for i in range(len(self._listeners)):
+            self._listeners[i].keypress(obj)
+
+    @_sio.event
+    def disconnect():
+        print("disconnected")
+
+    def send_data(self, dataframe):
+        if self._sio is None:
+            self._sio.emit('gamegrid:recv', dataframe)
+
+    def close(self):
+        self._sio.disconnect()
+        self._sio.wait()
+
+
+
+
 
 
 
