a4c022b7cd4f6a1f69a2a7d6b79d5d945b8f6c96
mmcquaig <mmcquaig@uncc.edu>
2019-08-12 10:32:29 -0400

    added docs to array 1D

--- bridges/array1d.py
+++ bridges/array1d.py
@@ -2,8 +2,33 @@ from bridges.array import *
 
 
 class Array1D(Array):
+    """
+    @brief This is a class can be used to create 1 dimensional arrays of type Element<E>.
 
-    def __init__(self, sz = None):
+    @author 	Kalpathi Subramanian, Matthew McQuaigue
+
+    @date  	7/18/19
+
+ 	This class can be used to create 1D arrays of type Element<E>  where E
+ 	is a generic object representing application specific data.
+
+    Array1D has iterator semantic to enable range for loops. For instance,
+
+    \code{java}
+    Array1D<Integer> arr = something();
+    for (Integer i : arr)
+        System.out.println(i);
+    \endcode
+
+    Example Tutorial at: http://bridgesuncc.github.io/tutorials/Array.html (1D, 2D, and 3D Array)<br>
+    """
+
+    def __init__(self, sz=None):
+        """
+        Create a 1D array object
+        Args:
+            sz: number of elements in the array
+        """
         super(Array1D, self).__init__()
         if sz is not None:
             self.size = sz
