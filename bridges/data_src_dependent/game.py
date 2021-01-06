2c17f619a0fc211b7feb55c3e240823304c7de5d
Kalpathi Subramanian <krs@uncc.edu>
2021-01-06 17:27:20 -0500

    python docs update

--- bridges/data_src_dependent/game.py
+++ bridges/data_src_dependent/game.py
@@ -1,87 +1,100 @@
 
-##
-#    @brief  A Game object, used along with the Games data source.
-#    
-#    This is a convenience class provided for  users who wish to use this
-#    data source as part of their application. It provides an API that makes
-#    it easy to access the attributes of this data set.
-#    
-#    Each game record has title, platform on which it can be played,
-#    rating,  and a list of genres.
-#    
-#    @author Matthew Mcquaigue, Kalpathi Subramanian
-#    @date   2/1/17, 12/29/20
-
-
 class Game:
-    ##
-    #     @brief Constructor
-    #    @param title  game title
-    #    @param rating  game rating
-    #    @param genre   game's genres
-    #
-    def __init__(self, title: str = "", platform: str = "", rating: float = 0.0, genre: str = ""):
-            self._title = title
-            self._platform = platform
-            self._rating = rating
-            self._genre = genre
+    """
+    @brief  A Game object, used along with the Games data source.
+    
+    This is a convenience class provided for  users who wish to use this
+    data source as part of their application. It provides an API that makes
+    it easy to access the attributes of this data set.
+    
+    Each game record has title, platform on which it can be played,
+    rating,  and a list of genres.
+    
+    @author Matthew Mcquaigue, Kalpathi Subramanian
+    @date   2/1/17, 12/29/20, 1/6/21
+    """
 
-    ##
-    # get game title
-    #
+    def __init__(self, title: str = "", platform: str = "", rating: float = 0.0, genre: str = ""):
+        """
+        @brief Constructor
+        Args:
+            title:  game title
+            rating:  game rating
+            genre:   game's genres
+        """
+        self._title = title
+        self._platform = platform
+        self._rating = rating
+        self._genre = genre
+
     @property
     def title(self):
+        """
+        @brief get game title
+        Returns:
+            game title
+        """
         return self._title
 
-    ##
-    # set game title
-    # @param t  title of game to set
-    #
     @title.setter
     def title(self, t):
+        """
+        @brief Set game title
+        Args:
+            t: game title to set
+        """
         self._title = t
 
-    ##
-    # get game platform
-    #
     @property
     def platform(self):
+        """
+        @brief get game platform
+        Returns:
+            game platform
+        """
         return self._platform
 
-    ##
-    # set game platform
-    # @param p  platform of game to set
-    #
     @platform.setter
     def platform(self, p):
+        """
+        @brief Set game platform
+        Args:
+            p: game platform to set
+        """
         self._platform = p
 
-    ##
-    # get game rating
-    #
     @property
     def rating(self):
+        """
+        @brief get game rating
+        Returns:
+            game rating
+        """
         return self._rating
 
-    ##
-    # set game rating
-    # @param r  rating of game to set
-    #
     @rating.setter
     def rating(self, r):
+        """
+        @brief Set game rating
+        Args:
+            r: game rating to set
+        """
         self._rating = r
 
-    ##
-    # get game genres (list of strings)
-    #
     @property
     def genre(self):
+        """
+        @brief get game genres
+        Returns:
+            game genres (list of strings)
+        """
         return self._genre
 
-    ##
-    # set game genres (list of strings)
-    # @param g  genres of game to set (list of strings)
-    #
     @genre.setter
     def genre(self, g):
+        """
+        @brief Set game title
+        Args:
+            g:  game genres to set
+        """
         self._genre = g
--- bridges/data_src_dependent/movie_actor_wiki_data.py
+++ bridges/data_src_dependent/movie_actor_wiki_data.py
@@ -1,76 +1,91 @@
 
-##    @brief This is a helper class for accessing actor-movie data from Wikidata.
-#
-#    @author Erik Saule, Matthew Mcquaigue, Kalpathi Subramanian
-#    @date  2020, 
-#
-class MovieActorWikiData:
 
-    ##
-    #     Constructor
-    #
+class MoviegctorWikiData:
+    """
+    @brief This is a helper class for accessing actor-movie data from Wikidata.
+    
+    @author Erik Saule, Matthew Mcquaigue, Kalpathi Subramanian
+    @date  2020, 1/6/21
+    """
+
     def __init__(self):
+        """
+        Constructor
+        """
         self._movie_uri = ""
         self._actor_uri = ""
         self._movie_name = ""
         self._actor_name = ""
+       
 
-    ##
-    # get movie uri
-    #
     @property
     def movie_uri(self):
+        """
+        @brief Return the Movie's URI
+        Returns:
+             movie uri
+        """
         return self._movie_uri
 
-    ##
-    # set movie uri
-    # @param uri uri of movie
-    #
     @movie_uri.setter
     def movie_uri(self, uri):
+        """
+        @brief Set the Movie's URI
+        Args:
+             uri: Movie's URI to set 
+        """
         self._movie_uri = uri
 
-    ##
-    # get actor uri
-    #
     @property
     def actor_uri(self):
+        """
+        @brief Return the actor's URI
+        Returns:
+             actor uri
+        """
         return self._actor_uri
 
-    ##
-    # set actor uri
-    # @param uri uri of actor
-    #
     @actor_uri.setter
     def actor_uri(self, uri):
+        """
+        @brief Set the actor's URI
+        Args:
+             uri: actor's URI to set 
+        """
         self._actor_uri = uri
 
-    ##
-    # get movie name
-    #
     @property
     def movie_name(self):
+        """
+        @brief Return the Movie's name
+        Returns:
+             movie name
+        """
         return self._movie_name
 
-    ##
-    # set movie name
-    # @param name name of movie
-    #
     @movie_name.setter
     def movie_name(self, name):
+        """
+        @brief Set the Movie's name
+        Args:
+             name: Movie's name to set 
+        """
         self._movie_name = name
 
-    ##
-    # get actor name
-    #
     @property
     def actor_name(self):
+        """
+        @brief Return the actor's name
+        Returns:
+             actor name
+        """
         return self._actor_name
 
-    ##
-    # set actor name
-    # @param name name of actor
-    #
     @actor_name.setter
     def actor_name(self, name):
+        """
+        @brief Set the actor's name
+        Args:
+             name: actor name to set 
+        """
         self._actor_name = name
