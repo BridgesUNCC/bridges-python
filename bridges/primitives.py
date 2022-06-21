145eca376d248228afceb8dbb8b901b4919b6ef6
quaiquai <mmcquaig@uncc.edu>
2022-06-21 13:07:24 -0400

    changes to 3d objects and test for other pc

--- bridges/primitives.py
+++ bridges/primitives.py
@@ -6,9 +6,13 @@ class Primitives:
     def __init__(self, type: str, position: list = None):
         self._type = type
         self._position = position
+        self.transform = []
+        self._color = "red"
         self._object_json = {
-            'type': self._type
-
+            'type': self._type,
+            'position': self._position,
+            'transform': self.transform,
+            'color': self._color
         }
 
     def push_representation(self, scene_json):
