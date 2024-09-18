19fb3d1e80d4c33e2aadbebb34cfc9c66c3007ef
Erik Saule <esaule@charlotte.edu>
2024-09-18 15:19:27 -0400

    Functional bar chart implementation
    no documentation yet.

--- bridges/bar_chart.py
+++ bridges/bar_chart.py
@@ -2,17 +2,46 @@ import json
 
 
 class BarChart:
+    """
+    @brief Support for drawing Bar charts.
+ 
+    Bar charts (https://en.wikipedia.org/wiki/Bar_chart) are used to
+    represent categorical data as a series of rectangular bars with length
+    proportional to the values they represent.
+    
+    Series in a bar chart provides data for a number of categories
+    (sometimes called bins). Categories are defined using
+    setCategories() and the series are added using addDataSeries().
+    The series are rendered in the order in which they were added. Once
+    a series has been added, it can not be modified.
+    
+    One should always define the categories before adding data. Changing the
+    categories after series have been added will throw exceptions;
+    adding series with different number of values than the number of
+    categories will throw an exception.
+    
+    The Bar charts can have a title, subtitle. The charts can be
+    horizontal or vertically oriented, using setBarOrientation().
+    
+    A tooltip indicating the value of a series in a particular bin is
+    displayed by hovering on a bar. One can append a string to the
+    value using setTooltipSuffix() to specify units in the tooltip if desired.
+    
+      @sa See tutorial on using BarChart at:
+       https://bridgesuncc.github.io/tutorials/BarChart.html
+    """
+
     # constructor method
 
     def __init__(self):
-        self.x_label = ""
-        self.y_label = ""
-        self.title = ""
-        self.sub_title = ""
-        self.tt_suffix = ""
-        self.direction = "horizontal"
-        self.series_data = {}
-        self.series_bins = []
+        self._c_label = ""
+        self._v_label = ""
+        self._title = ""
+        self._sub_title = ""
+        self._tt_suffix = ""
+        self._orientation = "horizontal"
+        self._series_data = []
+        self._cats = []
 
     # simple getters and setters to access attributes
     def get_data_structure_type(self):
@@ -21,78 +50,73 @@ class BarChart:
 
     @property
     def title(self) -> str:
-        return self.plot_title
+        return self._title
 
     @title.setter
     def title(self, plot_title: str) -> None:
-        self.plot_title = plot_title
+        self._title = plot_title
 
     @property
     def sub_title(self) -> str:
-        return self.plot_sub_title
+        return self._sub_title
 
     @sub_title.setter
     def sub_title(self, plot_sub_title: str) -> None:
-        self.plot_sub_title = plot_sub_title
+        self._sub_title = plot_sub_title
 
     @property
-    def series_label(self) -> str:
-        return self.y_label
+    def value_label(self) -> str:
+        return self._v_label
 
-    @series_label.setter
-    def series_label(self, y_label: str) -> None:
-        self._y_label = y_label
-
-    @property
-    def bins_label(self) -> str:
-        return self.x_label
-
-    @bins_label.setter
-    def bins_label(self, x_label: str) -> None:
-        self.x_label = x_label
+    @value_label.setter
+    def value_label(self, value_label: str) -> None:
+        self._v_label = value_label
 
     @property
-    def orientation(self, o) -> str:
-        self.direction = o
+    def categories_label(self) -> str:
+        return self._c_label
+
+    @categories_label.setter
+    def categories_label(self, c_label: str) -> None:
+        self._c_label = c_label
+
+    @property
+    def orientation(self) -> str:
+        return self._orientation
 
     @orientation.setter
-    def orientation(self) -> str:
-        return self.alignment
+    def orientation(self, orient) -> str:
+        self._orientation = orient
 
     @property
     def tooltip_suffix(self) -> str:
-        return self.tt_suffix
+        return self._tt_suffix
 
     @tooltip_suffix.setter
     def tooltip_suffix(self, suffix: str) -> None:
-        self.tt_suffix = suffix
+        self._tt_suffix = suffix
 
     @property
-    def series_data(self):
-        return self.series_data
+    def categories(self):
+        return self._cats
 
-    @series_data.setter
-    def series_data(self, series_name: str, data: float) -> None:
-        self.series_data[series_name] = data
+    @categories.setter
+    def categories(self, series_bins) -> None:
+        self._cats = series_bins
 
-    @property
-    def series_bins(self):
-        return self.series_bins
-
-    @series_bins.setter
-    def series_bins(self, series_bins) -> None:
-        self.series_bins = series_bins
+    def add_data_series(self, name, values) -> None:
+        self._series_data.append ((name, values))
 
     #Gets the data structure representation as a JSON string
     #Returns: data structure reprsentation (json string)
     def get_data_structure_representation(self):
         bins = {
             "xAxis": {
-                "categories": self.get_series_bins()
+                "categories": self.categories
             }
         }
         series = []
-        for key, value in self.get_series_data().items():
+        for key, value in self._series_data:
             temp_dict = {
                 "name": key,
                 "data": value
@@ -100,13 +124,13 @@ class BarChart:
             series.append(temp_dict)
         print(series)
         json_dict = {
-            "plot_title": self.get_title(),
-            "subtitle": self.get_sub_title(),
-            "x_label": self.get_bins_label(),
-            "y_label": self.get_series_label(),
-            "tooltip_suffix": self.get_tooltip_suffix(),
-            "alignment": self.orientation(),
-            "xaxis_data": bins,
+            "plot_title": self.title,
+            "subtitle": self.subtitle,
+            "x_label": self.categories_label,
+            "y_label": self.value_label,
+            "tooltip_suffix": self.tooltip_suffix,
+            "alignment": self.orientation,
+            "xaxis_data": { "xAxis": {"categories": self.categories} },
             "yaxis_data": {"series": series}
         }
         json_output = json.dumps(json_dict)
