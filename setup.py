ebdeeaea5647293cdae1295b80b1123f1cdfc3c3
Erik Saule <esaule@uncc.edu>
2025-01-11 01:29:39 -0500

    release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bridges",
-    version="3.4.5.dev2",
+    version="3.4.5",
     author="BRIDGES",
     author_email="bridges.uncc@gmail.com",
     description="Bridging Real-world Infrastructure Designed to Goal-align, Engage, and Stimulate, an NSF TUES",
